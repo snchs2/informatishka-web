765f2ccd4447479b9af326f6908bec3d47358e1a
Snchs <cazhdanov@mail.ru>
2021-12-21 11:55:55 +0400

    adaptive main part (1200, 1000, 768, 425)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const iconMenu = document.querySelector('.header_burger');
 if (iconMenu) {
     const menuBody = document.querySelector('.menu_body');
+    const Body = document.querySelector('.body');
     iconMenu.addEventListener("click", function(e) {
         iconMenu.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
+        Body.classList.toggle('_lock');
     });
 
 }
