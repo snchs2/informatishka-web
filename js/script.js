ae0495c63fdd7a61f8de9fc915cf97b187ba7fa2
Snchs <cazhdanov@mail.ru>
2021-12-22 16:24:16 +0400

    adaptive about_project (smooth scroll)

--- js/script.js
+++ js/script.js
@@ -8,5 +8,34 @@ if (iconMenu) {
         menuBody.classList.toggle('_active');
         Body.classList.toggle('_lock');
     });
-
 }
+
+
+function scrollTo(element) {
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: "smooth"
+    });
+}
+// переход в раздел о проекте
+var a_about_project = document.querySelector('#a_about_project');
+var about_project = document.querySelector('.about_project');
+a_about_project.addEventListener('click', () => {
+    scrollTo(about_project);
+    const Body = document.querySelector('.body');
+    const menuBody = document.querySelector('.menu_body');
+    Body.classList.remove('_lock');
+    menuBody.classList.remove('_active');
+})
+
+// переход в раздел главная
+var a_main = document.querySelector('#a_main');
+var main = document.querySelector('.main');
+a_main.addEventListener('click', () => {
+    scrollTo(main);
+    const Body = document.querySelector('.body');
+    const menuBody = document.querySelector('.menu_body');
+    Body.classList.remove('_lock');
+    menuBody.classList.remove('_active');
+})
